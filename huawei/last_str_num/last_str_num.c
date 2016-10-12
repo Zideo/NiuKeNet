dabc08654ee0c62bd8d29b0cb424f24164375f28
Zideo <763233094@qq.com>
2016-10-12 21:47:43 +0800

    add huawei/last_str_num

---
+++ huawei/last_str_num/last_str_num.c
@@ -0,0 +1,17 @@
+nclude<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    char str[5000];
+    
+    while(cin>>str);
+    
+    int str_len;
+    str_len = strlen(str);
+    
+    cout<<str_len<<endl;
+    
+    return 0;
+}
